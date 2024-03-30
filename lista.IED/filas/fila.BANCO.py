5565aaea0e2aca31ac0120c4b4a490bbfe42afaf
Annaaa <140774860+srtaayanamy@users.noreply.github.com>
2024-03-30 17:03:25 -0300

    Update fila.BANCO.py

--- lista.IED/filas/fila.BANCO.py
+++ lista.IED/filas/fila.BANCO.py
@@ -1,41 +1,54 @@
-# Implemente uma classe FilaDeBanco que simule a chegada e o atendimento de clientes. Utilize métodos para inserir clientes na fila, atender clientes e mostrar o estado atual da fila.
+# Simulação de Fila de Banco: 
+'''Implemente uma classe FilaDeBanco que simule a chegada e o atendimento de clientes. Utilize métodos para inserir clientes na fila, atender clientes e mostrar o estado atual da fila.'''
 
-class FilaDeBanco():
+class FilaDeBanco:
     def __init__(self):
-        self.clientes = []
+        self.fila = []
     
-    def enqueue(self, pessoa):
-        self.clientes.append(pessoa)
-        print('O cliente foi adicionado na fila.')
+    def enqueue (self, pessoa):
+        self.fila.append(pessoa)
+        print(f'A PESSOA {pessoa} FOI ADICIONADA À FILA.')
 
-    def dequeue(self):
-        if self.clientes:
-            pessoa = self.clientes.pop(0)
+    def dequeue (self):
+        if self.fila:
+            pessoa = self.fila.pop(0)
             print(f'O(a) cliente {pessoa} foi atendido(a).')
         else:
-            print('A fila está vazia.')
-
-    def mostrar(self, pessoa):
-        if self.clientes:
-            for pessoa in self.clientes:
-                print('-', pessoa)
-        else:
-            print('A fila está vazia.')
+            print('A FILA NÃO EXISTE.')
     
-clientes = FilaDeBanco()
-while True:
-    esc = input('O que deseja fazer?\n1-Inserir cliente;\n2-Atender Cliente;\n3-Mostrar a fila.\n4-Sair\n')
-    if esc == '1':
-        pessoa = input('Digite o cliente que deseja adicionar: ')
-        clientes.enqueue(pessoa)
-    elif esc == '2':
-        clientes.dequeue()
-    elif esc == '3':
-        clientes.mostrar(pessoa)
-    elif esc == '4':
-        print('Encerrando a fila.')
-        break
-    else:
-        print('Essa opção não existe')
+    def is_empty(self):
+        if not self.fila:
+            return True 
+        else:
+            return False 
 
-# isso aqui foi de um terror, de um desespero sem precedentes 
+    def mostrar (self):
+            print('\nFILA DE PESSOAS:\n')
+            print(self.fila)
+
+fila_clientes = FilaDeBanco()
+while True:
+
+    print('\nESCOLHA O QUE DESEJA FAZER:\n [1]Adicionar pessoas na fila\n [2]Atender alguém na fila\n [3]Mostrar Lista\n [4]Sair')
+    esc = int(input())
+
+    if esc == 1:
+        pessoa = input('DIGITE A PESSOA QUE DESEJA ADICIONAR À FILA: ')
+        fila_clientes.enqueue(pessoa)
+    
+    elif esc == 2:
+        fila_clientes.dequeue()
+
+    elif esc == 3:
+        fila_clientes.mostrar()
+        res = fila_clientes.is_empty() 
+        if res == True:
+            print('A FILA ESTÁ VAZIA.')
+
+    elif esc == 4:
+        print('ENCERRANDO PROGRAMA...')
+        break
+
+    else:
+        print('OPÇÃO INVÁLIDA, por favor escolha uma das opções listadas.')
+        
