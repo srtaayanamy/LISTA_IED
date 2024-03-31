bbf67c4f0b4ac56a2fe97b4f16f8b6e351368cd9
Annaaa <140774860+srtaayanamy@users.noreply.github.com>
2024-03-30 21:44:02 -0300

    Update fila.REVERSO.py

--- lista.IED/filas/fila.REVERSO.py
+++ lista.IED/filas/fila.REVERSO.py
@@ -1,35 +1,46 @@
-# Utilizando apenas operações de fila (enfileirar e desenfileirar), escreva uma função que inverta a ordem dos elementos de uma fila.
+# Reverso de Fila: 
+'''Utilizando apenas operações de fila (enfileirar e desenfileirar), escreva uma função que inverta a ordem dos elementos de uma fila.'''
 
-class Fila:
+class Ordem_Fila:
     def __init__(self):
-        self.filinha = []
-    
-    def enfilheirar (self, item):
-        self.filinha.append(item)
-    
+        self.fila = []
+
+    def enqueue (self, item):
+        self.fila.append(item)
+
     def desenfileirar (self):
-        if self.filinha:
-            self.filinha.reverse()
-            return self.filinha
-        else:
-            return False
+        stack = []
+        while self.fila:
+            stack.append(self.fila.pop(0))
+        while stack:
+            self.fila.append(stack.pop())
+        return self.fila
         
-filinha = Fila()
+    def mostrar(self):
+        if self.fila:
+            print('\nFILA ATUAL:')
+            print(self.fila)
+        else:
+            print('A FILA NÃO EXISTE.')
+        
+main = Ordem_Fila()
 while True:
 
-    print('Deseja fazer o quê?\n 1-Adicionar na lista;\n 2-Inverter a lista')
+    print('\nESCOLHA O QUE DESEJA FAZER:\n [1]Adicionar itens na fila\n [2]Inverter fila\n [3]Mostrar fila\n [4]Sair')
     esc = int(input())
 
     if esc == 1:
-        item = input('Digite o item: ')
-        filinha.enfilheirar(item)
-        print('Item enfileirado')
+        item = input('QUAL O ITEM QUE DESEJA ADICIONAR? ')
+        main.enqueue(item)
+        print('ITEM ADICIONADO À FILA.')
 
     elif esc == 2:
-        inverso = filinha.desenfileirar()
-        if inverso != False:
-            print(inverso)
-        else:
-            print('Não tem fila')
+        retorno = main.desenfileirar()
+        print('\nFILA INVERSA:')
+        print(retorno)
+
+    elif esc == 3:
+        main.mostrar()
+    
     else:
-        break
+        print('OPÇÃO INVÁLIDA, por favor escolha dentre as opções listadas.')
