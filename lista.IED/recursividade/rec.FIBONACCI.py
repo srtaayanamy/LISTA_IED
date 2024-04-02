a80181793279d71934edbf967b80edb9d74b8bb6
Annaaa <140774860+srtaayanamy@users.noreply.github.com>
2024-04-01 21:24:02 -0300

    Update rec.FIBONACCI.py

--- lista.IED/recursividade/rec.FIBONACCI.py
+++ lista.IED/recursividade/rec.FIBONACCI.py
@@ -1,16 +1,18 @@
-# Implemente uma função recursiva que retorne o N-ésimo termo da sequência de Fibonacci. Teste a função com valores de N pequenos e grandes e observe a performance.
+# Fibonacci:
+'''Implemente uma função recursiva que retorne o N-ésimo termo da sequência de Fibonacci. Teste a função com valores de N pequenos e grandes e observe a performance.'''
 
-
-def f(n):
-
+def fibonacci (n):
     if n == 1:
         return (0)
-    if n == 2:
+    elif n == 2:
         return (1)
     if n > 2:
-        return (f(n-1) + f(n-2))
+        return (fibonacci(n-1) + fibonacci(n-2))
     
-n = int(input('Quantos elementos? '))
+print('QUAL O N-ÉSIMO TERMO? (Digite um número inteiro.)\n')
+n = int(input())
 
-for i in range(1, n):
-        print(f(i))
+print('O TERMO É {}!'.format(fibonacci(n)))
+print('LISTA COMPLETA ATÉ O N-ÉSIMO TERMO:\n')
+for i in range(1, n + 1):
+     print(fibonacci(i))
